c0b26f5f06b8b6de8bda6c29572e698eaf8d2c41
Natalie <natalie.todd15@gmail.com>
2018-09-20 22:11:04 -0600

    outcome is here

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Outcome from './Outcome';
 
 const inter = () => {
     return (
         <div>
-        {console.log('interface is here')}
+            {console.log('interface is here')}
+            <Outcome />
+            <div>
+                <a>Dealer Score : </a>
+                <a>Player Score : </a>
+            </div>
+            <button >Deal</button>
+            <button>Hit</button>
+            <button >Stand</button>
         </div>
     )
 }
---
+++ src/components/Outcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const outcome = () => {
+    return (
+        <div>
+            {console.log('outcome is here')}
+            <div>
+                Click deal to start!
+            </div>
+        </div>
+    )
+}
+
+export default outcome;
