b4323b72b259a58671d5cff982cbcbed97a8c6ed
Natalie <natalie.todd15@gmail.com>
2018-09-22 07:45:05 -0600

    hand props being passed down from table state

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Card from './Card';
 
-const hand = () => {
+const hand = (props) => {
     return (
         <div>
             {console.log('hand is here')}
             <Card />
+            <p>This is {props.test}</p>
             <Card />
         </div>
     )
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,14 +3,24 @@ import Hand from './Hand';
 import Interface from './Interface';
 
 class Table extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            suits: ['♣', '♦', '♥', '♠'],
+            values: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
+            colors: [
+                { '♣': 'black', '♦': 'red', '♥': 'red', '♠': 'black' }
+            ]
+        }
+    }
     render() {
         return (
             <div>
                 {console.log('table is here')}
-                <Hand />
+                <Hand test={this.state.values[2]} />
                 <Interface />
-                <Hand />
-                </div>
+                <Hand test={this.state.values[4]}/>
+            </div>
         )
     }
 }
