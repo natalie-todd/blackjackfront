e89d24578fe7921ff630b9547d9c68df14fd7d98
Natalie <natalie.todd15@gmail.com>
2018-09-23 17:19:22 -0600

    switched to the store, working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import { fromJS, Map } from 'immutable';
-import { newDeck, deal } from './Library/Cards';
+// import { fromJS, Map } from 'immutable';
+// import { newDeck, deal } from './Library/Cards';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -18,29 +18,29 @@ let store = createStore(reducer);
 store.dispatch(setupGame());
 store.dispatch(setRecord(0, 0));
 
-// const deck = newDeck();
-// console.log(deck);
+// // const deck = newDeck();
+// // console.log(deck);
 
-// ...
+// // ...
 
-let deck = newDeck();
-let playerHand, dealerHand;
+// let deck = newDeck();
+// let playerHand, dealerHand;
 
-[deck, playerHand] = deal(deck, 2);
-[deck, dealerHand] = deal(deck, 1);
+// [deck, playerHand] = deal(deck, 2);
+// [deck, dealerHand] = deal(deck, 1);
 
-dealerHand = dealerHand.push(new Map());
+// dealerHand = dealerHand.push(new Map());
 
-const state = fromJS({
-    deck,
-    playerHand,
-    dealerHand,
-    "winCount": 0,
-    "lossCount": 0,
-    hasStood: false
-});
+// const state = fromJS({
+//     deck,
+//     playerHand,
+//     dealerHand,
+//     "winCount": 0,
+//     "lossCount": 0,
+//     hasStood: false
+// });
 
-console.log(state);
+// console.log(state);
 
 // ...
 // console.log("start deck:");
