0ddb38542147cf47f1c23e93b2c74ac51455e31f
Natalie <natalie.todd15@gmail.com>
2018-09-22 19:07:09 -0600

    randomly generating numbers when clicking on shuffleSuits

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,7 +6,7 @@ class Table extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            suits: ['♣', '♦', '♥', '♠'],
+            suits: ['clubs', 'diamonds', 'hearts', 'spades'],
             values: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
             // colors: [
             //     { id: 1, clubs: 'black', diamonds: 'red', hearts: 'red', spades: 'black' }
@@ -15,24 +15,26 @@ class Table extends Component {
         }
     }
 
-    shuffleSuits = (suits) => {
+    shuffleSuits = () => {
+        let oldSuits = this.state.suits;
+        let newSuits = [];
         let i = 0, j = 0, temp = null;
-        for (i = suits.length - 1; i > 0; i -= 1) {
+        for (i = oldSuits.length - 1; i > 0; i -= 1) {
             j = Math.floor(Math.random() * (i + 1))
-            temp = suits[i]
-            suits[i] = suits[j]
-            suits[j] = temp
+            temp = oldSuits[i]
+            oldSuits[i] = oldSuits[j]
+            oldSuits[j] = temp
+            newSuits.push(j);
         }
-        this.setState({ suits: suits })
-        console.log(suits);
+        console.log(newSuits)
     }
-    
-    dealCards = () => {
-        // shuffleSuits(this.state.suits)
-        const doesShow = this.state.showCards;
-        this.setState({ showCards: !doesShow })
-        console.log(this.state.suits)
-    }
+
+    // dealCards = () => {
+    //     // shuffleSuits(this.state.suits)
+    //     const doesShow = this.state.showCards;
+    //     this.setState({ showCards: !doesShow })
+    //     console.log(this.state.suits)
+    // }
 
     render() {
         let cards = null;
@@ -48,7 +50,7 @@ class Table extends Component {
                 <Interface
                     test={this.state.values}
                     suit={this.state.suits}
-                    clicked={this.dealCards} />
+                    clicked={this.shuffleSuits} />
                 {cards}
                 {console.log('table is here')}
             </div>
