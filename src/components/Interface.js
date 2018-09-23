855ea7f28cee07b1afbae156e48fb4d9888c3500
Natalie <natalie.todd15@gmail.com>
2018-09-23 13:34:18 -0600

    have buttons on the interface, moving onto has stood next

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -4,16 +4,23 @@ import React from 'react';
 import Outcome from './Outcome';
 import { Alert, Row, Col, Button } from 'reactstrap';
 
-const inter = (props) => {
-    return (
-        <div className='interface'>
-            {console.log('interface is here')}
-            <Outcome />
-            <div>
-                <p>This is the info component</p>
-            </div>
-        </div>
-    )
-}
+export default class Interface extends React.Component {
+    render() {
+        return (
+            <div className='interface'>
+                {console.log('interface is here')}
+                <Outcome />
+                <div className='oInterface'>
+                    <span id="player_record">
+                        Wins: {this.props.winCount} Losses: {this.props.lossCount}
+                    </span>
+                    <span id="buttons">
+                        <Button color="danger">Hit</Button>
+                        <Button color="success">Stand</Button>
+                    </span>
 
-export default inter;
+                </div>
+            </div>
+        )
+    }
+}
