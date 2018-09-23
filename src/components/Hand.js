d5771a07ebd5a85b998bdd42e82f4b7bdf7e4cb2
Natalie <natalie.todd15@gmail.com>
2018-09-23 14:10:56 -0600

    cards are looking a bit card like

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Card from './Card';
 
-const hand = (props) => {
-    return (
-        <div>
-            <p>This hand is a hand component.</p>
-        </div>
-    )
+export default class Hand extends React.Component {
+    render() {
+        return (
+            <div className='hand'>
+                {this.props.cards.map((card, i) =>
+                    <Card suit={card.get('suit')}
+                        rank={card.get('rank')}
+                        key={i} />
+                )}
+            </div>
+        )
+    }
 }
-
-export default hand;
