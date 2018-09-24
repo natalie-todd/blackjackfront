944e1dcb1b545fed435a63abd777e6259103c634
Natalie <natalie.todd15@gmail.com>
2018-09-23 20:40:10 -0600

    when you press hit you get another card

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -3,11 +3,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'reactstrap';
+import { dealToPlayer } from '../Action_creators';
 
 export class Interface extends React.Component {
     render() {
         return (
-            <div className='interface'>
+            <div id='interface'>
                 {console.log('interface is here')}
                 <div className='oInterface'>
                     <span id="player_record">
@@ -16,7 +17,8 @@ export class Interface extends React.Component {
                     <span id="buttons">
                         <Button
                             color="danger"
-                            disabled={this.props.hasStood}>Hit</Button>
+                            disabled={this.props.hasStood}
+                            onClick={this.props.onClickHit}>Hit</Button>
                         <Button
                             color="success"
                             disabled={this.props.hasStood}>Stand</Button>
@@ -34,5 +36,12 @@ function mapStateToProps(state) {
         hasStood: state.get('hasStood')
     };
 }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onClickHit: () => {
+            dispatch(dealToPlayer())
+        }
+    }
+}
 
-export const InterfaceContainer = connect(mapStateToProps)(Interface);
+export const InterfaceContainer = connect(mapStateToProps, mapDispatchToProps)(Interface);
