828d01036974894170e7ce1cd720f68ad2dfe256
Natalie <natalie.todd15@gmail.com>
2018-09-23 18:56:15 -0600

    changed app to a container component, working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import Interface from './components/Interface';
+// import Interface from './components/Interface';
 import Hand from './components/Hand';
 import './App.css';
 import { InterfaceContainer } from './components/Interface';
+import { connect } from 'react-redux';
 
-export default class App extends React.Component {
+export class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>React Blackjack</h1>
         <InterfaceContainer />
         <h1>Player's hand:</h1>
-        <Hand cards={this.props.state.get('playerHand')} />
+        <Hand cards={this.props.playerHand} />
         <h1>Dealer's hand:</h1>
-        <Hand cards={this.props.state.get('dealerHand')} />
+        <Hand cards={this.props.dealerHand} />
       </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {
+      playerHand: state.get('playerHand'),
+      dealerHand: state.get('dealerHand')
+  };
+}
+
+export const AppContainer = connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './App';
+import { AppContainer } from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
 // import { fromJS, Map } from 'immutable';
 // import { newDeck, deal } from './Library/Cards';
 import registerServiceWorker from './registerServiceWorker';
@@ -18,45 +16,9 @@ let store = createStore(reducer, undefined, window.devToolsExtension ? window.de
 store.dispatch(setupGame());
 store.dispatch(setRecord(0, 0));
 
-// // const deck = newDeck();
-// // console.log(deck);
-
-// // ...
-
-// let deck = newDeck();
-// let playerHand, dealerHand;
-
-// [deck, playerHand] = deal(deck, 2);
-// [deck, dealerHand] = deal(deck, 1);
-
-// dealerHand = dealerHand.push(new Map());
-
-// const state = fromJS({
-//     deck,
-//     playerHand,
-//     dealerHand,
-//     "winCount": 0,
-//     "lossCount": 0,
-//     hasStood: false
-// });
-
-// console.log(state);
-
-// ...
-// console.log("start deck:");
-// console.log(deck);
-// console.log("end deck:");
-// console.log(deck);
-// console.log("playerHand:");
-// console.log(playerHand);
-// console.log("dealerHand:");
-// console.log(dealerHand);
-
-// // ...
-
 ReactDOM.render(
 <Provider store={store}>
-    <App state={store.getState()} />
+    <AppContainer />
 </Provider>, 
 document.getElementById('root'));
 registerServiceWorker();
